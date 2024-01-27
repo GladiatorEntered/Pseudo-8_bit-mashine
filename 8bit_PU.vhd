2e9d2c96ceae8a898e901fa9003b8daefd769190
GladiatorEntered <xkrasoo00@stud.fit.vutbr.cz>
2024-01-27 23:52:15 +0200

    Thanks SO

--- 8bit_PU.vhd
+++ 8bit_PU.vhd
@@ -3,16 +3,16 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity bit_PU is
-		port(operation : in std_logic_vector (2 downto 0);
-			 Clk	   : in std_logic;
-			 operand1  : in std_logic_vector (7 downto 0);
-	   		 operand2  : in std_logic_vector (7 downto 0);
-			 result	   : out std_logic_vector(7 downto 0)
-			 );	 
+	port(operation : in std_logic_vector (2 downto 0);
+		 Clk	   : in std_logic;
+		 operand1  : in std_logic_vector (7 downto 0);
+	 	 operand2  : in std_logic_vector (7 downto 0);
+		 result	   : out std_logic_vector(7 downto 0) 
+		 );	 
 end bit_PU;
 
 architecture proc of bit_PU is
-		function Add (op1 : std_logic_vector (7 downto 0);
+		function "+" (op1 : std_logic_vector (7 downto 0);
                       op2 : std_logic_vector (7 downto 0)) return std_logic_vector is
 		variable res: std_logic_vector (7 downto 0);  
 		begin
@@ -20,14 +20,42 @@ architecture proc of bit_PU is
 			return res;
 		end function;
 
+
+		function "-" (op1 : std_logic_vector (7 downto 0);
+                      op2 : std_logic_vector (7 downto 0)) return std_logic_vector is
+		variable res: std_logic_vector (7 downto 0);  
+		begin
+			if unsigned(op1) < unsigned(op2) then 	
+				res := std_logic_vector((unsigned(op2)-unsigned(op1)) mod 256);
+			else
+				res := std_logic_vector((unsigned(op1)-unsigned(op2)) mod 256);
+			end if;
+			return res;
+		end function;
+
 begin
 
-		process(Clk) is
-		begin
-			if rising_edge(Clk) then
-					result <= Add(operand1, operand2);
-			end if;
-		end process;
+	process(Clk) is --may cause errors in future
+	begin
+		case operation is
+			when "000"   =>
+					result <= operand1 + operand2;
+			when "001"   =>
+					result <= operand1 - operand2;	
+			when "010"   =>
+					result <= operand1 or operand2;
+		    when "011"   => 
+					result <= operand1 and operand2;
+			when "100"   =>
+					result <= operand1 nand operand2;
+			when "101"   =>
+					result <= operand1 xor operand2;
+			when "111"   =>
+					result <= x"FF";
+			when others  =>
+				result <= "UUUUUUUU";
+		end case;
+	end process;
 
 end architecture;
 
--- 8bit_Tb.vhd
+++ 8bit_Tb.vhd
@@ -7,49 +7,42 @@ end bit_Tb;
 
 architecture proc of bit_Tb is
 
-signal   Clk  : std_logic  := '0';
-constant Freq : integer    := 100;
-constant PeriodTime : time := 1000 ms / Freq;
-signal	 operation  : std_logic_vector (2 downto 0) := "000";
-signal   result     : std_logic_vector (7 downto 0);
-signal   operand1   : std_logic_vector (7 downto 0) := (others => '0');
-signal   operand2   : std_logic_vector (7 downto 0) := (others => '0');
-
-		component bit_PU
+		component bit_PU is
 			port(operation : in std_logic_vector (2 downto 0);
 				 Clk	   : in std_logic;
 				 operand1  : in std_logic_vector (7 downto 0);
 	   			 operand2  : in std_logic_vector (7 downto 0);
-			 	 result	   : out std_logic_vector(7 downto 0)
+				 result	   : out std_logic_vector(7 downto 0) 
 				 );
-		end component;
+		end component bit_PU;
 
+signal   Clk  : std_logic  := '0';
+constant Freq : integer    := 100;
+constant PeriodTime : time := 1000 ms / Freq;
+signal	 operation  : std_logic_vector (2 downto 0) := "001";
+signal   result     : std_logic_vector (7 downto 0) := (others => '0');
+signal   operand1   : std_logic_vector (7 downto 0) := (others => '0');
+signal   operand2   : std_logic_vector (7 downto 0) := (others => '0');
 begin
-i_bit_PU : bit_PU port map(Clk => Clk,
-		 operation => operation,
-		 operand1  => operand1,
-		 operand2  => operand2,
-		 result    => result);
 
-Clk <= not Clk after PeriodTime / 2;
+	i_bit_PU: component bit_PU
+   	port map(Clk => Clk,
+	 operation => operation,
+	 operand1  => operand1,
+	 operand2  => operand2,
+	 result    => result);
 
-process is
-begin
-report "HO-HO!";
-wait;
-end process;
+	Clk <= not Clk after PeriodTime / 2;
 
-process (Clk) is
-begin
-		report "YEAH";
+	process (Clk) is
+	begin
 		if rising_edge(Clk) then
-			if unsigned(operand1) > unsigned(operand2) then
+			operation <=std_logic_vector(unsigned(operation)+5);
+			if unsigned(operand1) < unsigned(operand2) or (operand1 = x"FF" and operand2 = x"00") then
 				operand1 <= std_logic_vector((unsigned(operand1)+1) mod 256);
 			else
 				operand2 <= std_logic_vector((unsigned(operand2)+1) mod 256);
 			end if;
-		else
-			report "NOPE";
 		end if;
 end process;
 
